f01a0bfd2f37dc9f1f7666c67ed2e5326961da43
shukla-sahil <87928951+shukla-sahil@users.noreply.github.com>
2021-07-27 17:46:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+/* input principal, rate and number of years, then calculate the interest */
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+	if (parseInt(principal) <= 0 || principal == "") {
+		alert("Enter a positive number");
+		eraseInterest();
+		document.getElementById("principal").focus();
+		return false;
+	}
+    var rate = document.getElementById("rate").value;	
+    var years = document.getElementById("years").value;
+		
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear()+ parseInt(years);
+	
+    document.getElementById("result").innerHTML = "If you deposit \<mark>" + principal + "\</mark>, <br\>at an interest rate of \<mark>" + rate + "\%.\</mark><br\>You will receive an amount of \<mark>" + interest + "\</mark>,<br\>in the year \<mark>" + year + "\</mark><br\>";
 }
-        
+
+/* update rate on slider */
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
+
+/* erase calculated interest*/
+function eraseInterest() 
+{
+	document.getElementById("principal").innerText = "";
+	document.getElementById("result").innerText = "";
+	
+}
